affc0307de51b027e488ce8578511ac95fe512c9
coha-kim <seoha.kim@mds.ac.nz>
2025-06-23 18:22:46 +1200

    watering full info page

--- javascript/full-info-care.js
+++ javascript/full-info-care.js
@@ -3,12 +3,20 @@ import { supabase } from "../src/connection.js";
 const { data, error } = await supabase
     .from("care-info")
     .select("*")
+      .order("id", { ascending: true })
 
-document.querySelector("#each-content").innerHTML = data.map(post =>
+document.querySelector("#all-content").innerHTML = data.map(post =>
     `
-    <article>
-    <h3>${post.title}</h3>
-    <p>${post.content}</p>
+   <article class="each-content">
+      ${post.image_filename ? `
+      <div class="image-container">
+        <img src="assets/${post.image_filename}" alt="${post.image_filename} class=images">
+      </div>
+      ` : ''}
+      <h3>${post.title}</h3>
+      <p>${post.content}</p>
     </article>
+
+    <img src="assets/circular-element.svg" alt="Circular graphic element" class="pink-element-content">
     `
 ).join('')
